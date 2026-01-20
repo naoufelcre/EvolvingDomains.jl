644122629d6b145139f33741c48bffab702601d5
Naoufel <cressonnaoufel@gmail.com>
2026-01-20 10:19:00 +0100

    roooh encore

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,32 @@
 using EvolvingDomains
 using Documenter
 
+using Literate
+
+# Generate examples
+examples_dir = joinpath(@__DIR__, "..", "examples")
+output_dir = joinpath(@__DIR__, "src", "examples")
+
+# Create output directory if it doesn't exist
+mkpath(output_dir)
+
+# List of examples to process
+example_files = [
+    "colliding_balls_minimal.jl",
+    # "csg.jl",
+    # "independent_motion_minimal.jl",
+    # "stokes_driven_minimal.jl",
+    # "zalesak_minimal.jl",
+]
+
+generated_examples = []
+
+for file in example_files
+    input_path = joinpath(examples_dir, file)
+    Literate.markdown(input_path, output_dir; documenter=true)
+    push!(generated_examples, "examples/$(replace(file, ".jl" => ".md"))")
+end
+
 DocMeta.setdocmeta!(EvolvingDomains, :DocTestSetup, :(using EvolvingDomains); recursive=true)
 
 makedocs(;
@@ -19,6 +45,7 @@ makedocs(;
             "Velocity" => "velocity.md",
             "Internals" => "internals.md",
         ],
+        "Examples" => generated_examples,
     ],
 )
 
